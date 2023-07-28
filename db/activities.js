4ac6f9509cb0c7d213eab220f1c39422a0480d9b
Craig Case <case2cn@gmail.com>
2023-07-27 18:33:57 -0600

    Completed the functions in db/activities

--- db/activities.js
+++ db/activities.js
@@ -1,4 +1,5 @@
 const client = require('./client');
+const { addActivityToRoutine } = require("./routine_activities");
 
 // database functions
 async function createActivity({ name, description }) {
@@ -15,7 +16,7 @@ async function createActivity({ name, description }) {
 async function getAllActivities() {
   // select and return an array of all activities
   const getActivitiesSql = `SELECT * FROM activities`;
-  const { rows: [activities],} = await client.query(getActivitiesSql,);
+  const { rows: activities,} = await client.query(getActivitiesSql,);
   return activities
 }
 
@@ -34,32 +35,28 @@ async function getActivityByName(name) {
 }
 
 // used as a helper inside db/routines.js
-async function attachActivitiesToRoutines(routines) {}
+async function attachActivitiesToRoutines(routines) {
+  
+}
 
 
 async function updateActivity({ id, ...fields }) {
   let setSql=''; 
-  
+  let data= []
+  //builed sql based on the field property
   if(fields.name && !fields.description) {
-    setSql = `name = "${fields.name}"`
+    setSql = `UPDATE activities SET name = $1 WHERE id = $2 RETURNING *;`;
+    data = [fields.name, id];
   } else if(!fields.name && fields.description) {
-    setSql = `description = "${fields.description}"`
+    setSql = `UPDATE activities SET description = $1 WHERE id = $2 RETURNING *;`;
+    data = [fields.description, id];
   } else if(fields.name && fields.description) {
-    setSql = `name = "${fields.name}", description = "${fields.description}"`
+    setSql = `UPDATE activities SET name = $1, description = $2 WHERE id = $3 RETURNING *;`;
+    data = [fields.name, fields.description, id];
   }
-  console.log("UPDATE_ACTIVITY_FIELDS-", setSql); 
-  const data = [setSql, id];
-
   
-  const sql = `UPDATE activities
-  SET $1 WHERE id = $3 RETURNING *;`
-  // don't try to update the id
-  // do update the name and description
-  // return the updated activity
-  
-  const {rows: [activity],} = await client.query(sql, data);
-  
-  
+  const { rows: [activity], } = await client.query(setSql, data);
+    
   return activity;
 }
 
