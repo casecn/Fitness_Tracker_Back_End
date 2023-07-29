be96fb761146baab16956de4a55177e114471919
Craig Case <case2cn@gmail.com>
2023-07-29 15:04:28 -0600

    Got all db functions to pass the 67 tests.

--- db/activities.js
+++ db/activities.js
@@ -36,9 +36,31 @@ async function getActivityByName(name) {
 
 // used as a helper inside db/routines.js
 async function attachActivitiesToRoutines(routines) {
+ 
+  if (!routines?.length) {
+    return [];
+  }  
   
-}
+  const placeHolders = routines.map((_, index) => `$${index + 1}`).join(", ")
+  const data = routines.map((routine) => routine.id);
+    
+  try {
+    const activitesSql = `SELECT activities.*, r_a.id AS "routineActivityId", duration, count,  "routineId"
+      FROM activities
+      LEFT JOIN routine_activities as r_a on r_a."activityId" = activities.id
+      WHERE "routineId" IN (${placeHolders});`;
+      //console.log(activitesSql)
+    const { rows: activities } = await client.query(activitesSql, data );
+    
+    routines.forEach((routine) => {
+      routine.activities = activities.filter((activity) => activity.routineId === routine.id)}); 
+    //console.log("ACTIVITIES_1223:", routines);
+    return routines;
 
+    } catch (error) {
+      console.erro("attachActivitiesToRoutines ERROR:", error);
+    }
+}
 
 async function updateActivity({ id, ...fields }) {
   let setSql=''; 
@@ -54,7 +76,6 @@ async function updateActivity({ id, ...fields }) {
     setSql = `UPDATE activities SET name = $1, description = $2 WHERE id = $3 RETURNING *;`;
     data = [fields.name, fields.description, id];
   }
-  
   const { rows: [activity], } = await client.query(setSql, data);
     
   return activity;
